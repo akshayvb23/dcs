8b4e670464a44b9000cd3cbd3d2b4d40b6c64773
akshayvb23 <55960098+akshayvb23@users.noreply.github.com>
2020-03-08 18:26:20 -0700

    Implemented storage of the mail in a structured manner in the HDFS

--- server.py
+++ server.py
@@ -1,41 +1,58 @@
 from __future__ import print_function
-from datetime import datetime
+import time
+import os
 import asyncore
 from smtpd import SMTPServer
 import subprocess
 
 def execute_shell_command(command):
-    print("Executing command: " + str(command))
     process = subprocess.Popen(command, stdout=subprocess.PIPE, universal_newlines=True)
-
-    while True:
-        output = process.stdout.readline()
-        print(output.strip())
-        # Do something else
-        return_code = process.poll()
-        if return_code is not None:
-            print('RETURN CODE', return_code)
-            # Process has finished, read rest of the output 
-            for output in process.stdout.readlines():
-                print(output.strip())
-            break
-
-class EmlServer(SMTPServer):
-    num = 0
-    def process_message(self, peer, sender_username, rcpttos, data, mail_options=None, rcpt_options=None):
-        filename = '%s-%d.eml' % (datetime.now().strftime('%Y%m%d%H%M%S'), self.num)
-        f = open(filename, 'w')
-        f.write(str(data))
-        f.close()
-        print('%s saved.' % filename)
-        self.num += 1
-        hdfs_user_dir = "/var/mail/" + sender_username
-        command = ['bin/hdfs', 'dfs', '-mkdir', hdfs_user_dir]
-        execute_shell_command(command)
+    output = process.stdout.readline()
+    return_code = process.poll()
+    if return_code == 0:
+        return True
+    return False
 
 
+class HDFS:
+    def check_if_directory_exists(self, directory):
+        command = ['bin/hdfs', 'dfs', '-test', '-d', directory]
+        return execute_shell_command(command)
+
+    def create_directory(self, directory):
+        command = ['bin/hdfs', 'dfs', '-mkdir', '-p', directory]
+        return execute_shell_command(command)
+
+    def store_file(self, filename, destination_directory):
+        command = ['bin/hdfs', 'dfs', '-put', filename, destination_directory]
+        return execute_shell_command(command)
+
+class EmailServer(SMTPServer):
+
+    hdfs = HDFS()
+    def create_sender_directory_for_receiver_if_not_exists(self, sender, receiver):
+        sender_directory = "/var/mail/" + receiver + "/" + sender
+        if self.hdfs.check_if_directory_exists(sender_directory):
+            return
+        self.hdfs.create_directory(sender_directory)
+
+    def store_mail_in_hdfs(self, sender, receiver, filename):
+        mail_directory = "/var/mail/" + receiver + "/" + sender
+        self.hdfs.store_file(filename, mail_directory)
+
+    def process_message(self, peer, sender, receivers_list, data, mail_options=None, rcpt_options=None):
+        filename = str(time.time()) + ".eml"
+        file_handle = open(filename, 'w')
+        file_handle.write(str(data))
+        file_handle.close()
+        for receiver in receivers_list:
+            self.create_sender_directory_for_receiver_if_not_exists(sender, receiver)
+            self.store_mail_in_hdfs(sender, receiver, filename)
+        os.remove(filename)
+
+        
 def run():
-    foo = EmlServer(('localhost', 1025), None)
+    foo = EmailServer(('localhost', 1025), None)
     try:
         asyncore.loop()
     except KeyboardInterrupt:
@@ -44,3 +61,4 @@ def run():
 
 if __name__ == '__main__':
     run()
+
